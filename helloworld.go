ac365c3336177241f35c5c2d0593b82fa9925d1b
DennyZhang <contact@dennyzhang.com>
2018-04-07 14:32:41 -0500

    add sample code

---
+++ helloworld.go
@@ -0,0 +1,21 @@
+//-------------------------------------------------------------------
+// @copyright 2017 DennyZhang.com
+// Licensed under MIT 
+//   https://www.dennyzhang.com/wp-content/mit_license.txt
+//
+// File: helloworld.go
+// Author : Denny <https://www.dennyzhang.com/contact>
+// Description : go run helloworld.go
+// --
+// Created : <2018-04-07>
+// Updated: Time-stamp: <2018-04-07 14:32:22>
+//-------------------------------------------------------------------
+package main
+
+import "fmt"
+func main() {
+	msg := "hello world\n";
+	// print sample message
+	fmt.Print(msg)
+}
+// File: helloworld.go ends
